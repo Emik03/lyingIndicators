9e65de9c0fb41fbaed4b6944802926b764c3e90a
Emik <emik.lakunishok@gmail.com>
2020-04-13 20:16:21 +0200

    Module

---
+++ Assets/Examples/1.2 Module2/ExampleModule2.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+public class ExampleModule2 : MonoBehaviour
+{
+    public KMAudio Audio;
+    public KMBombInfo Bomb;
+    public KMSelectable[] Buttons;
+
+    int _moduleId;
+    static int _moduleIdCounter;
+
+    private void Awake()
+    {
+        _moduleId = _moduleIdCounter++;
+        for (byte i = 0; i < Buttons.Length; i++)
+        {
+            var btn = Buttons[i];
+            btn.OnInteract += delegate 
+            {
+                HandlePress(i);
+                return false;
+            };
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void HandlePress(byte btn)
+    {
+
+    }
+}
